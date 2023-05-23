088dd40fba51055ce5e5341b74477c77bfcd413e
fatalryuu <vanyax941@gmail.com>
2023-05-23 12:25:38 +0300

    feat: Day of The Week (7)

--- script.js
+++ script.js
@@ -62,7 +62,18 @@ function isJubilee(year) {
 
 // Day of The Week (7)
 
-
+function getDayOfTheWeek(date) {
+    const day = new Date(date).getDay();
+    switch (day) {
+        case 0: return "Sunday"
+        case 1: return "Monday"
+        case 2: return "Tuesday"
+        case 3: return "Wednesday"
+        case 4: return "Thursday"
+        case 5: return "Friday"
+        case 6: return "Saturday"
+    }
+}
 
 // Is the date today (8)
 
