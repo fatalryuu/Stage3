7394fb51d4e779f5f53b9bd9a82adba8b54a9111
DrEiZ3R <vanyax941@gmail.com>
2023-05-24 22:15:00 +0300

    feat: Sum of differences in array (37)

--- script.js
+++ script.js
@@ -577,7 +577,18 @@ function logicalCalc(array, op){
 
 // Sum of differences in array (37)
 
+function sumOfDifferences(arr) {
+    if (arr.length === 0) {
+        return 0;
+    }
 
+    arr.sort((a, b) => b - a);
+    let sum = 0;
+    for (let i = 0; i < arr.length - 1; i++) {
+        sum += arr[i] - arr[i + 1];
+    }
+    return sum;
+}
 
 // Additive Numbers (38)
 
