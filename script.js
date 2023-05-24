bccece32f9a33bc4f8b18d98df74a38e09340cda
DrEiZ3R <vanyax941@gmail.com>
2023-05-24 19:29:37 +0300

    feat: Welcome! (26)

--- script.js
+++ script.js
@@ -405,7 +405,32 @@ function backToTheFuture (str) {
 
 // Welcome! (26)
 
-
+function greet(language) {
+    const languages = {
+        english: 'Welcome',
+        czech: 'Vitejte',
+        danish: 'Velkomst',
+        dutch: 'Welkom',
+        estonian: 'Tere tulemast',
+        finnish: 'Tervetuloa',
+        flemish: 'Welgekomen',
+        french: 'Bienvenue',
+        german: 'Willkommen',
+        irish: 'Failte',
+        italian: 'Benvenuto',
+        latvian: 'Gaidits',
+        lithuanian: 'Laukiamas',
+        polish: 'Witamy',
+        spanish: 'Bienvenido',
+        swedish: 'Valkommen',
+        welsh: 'Croeso'
+    }
+    if (languages[language]) {
+        return languages[language]
+    } else {
+        return languages["english"];
+    }
+}
 
 // Give me the k first digits. Odd digits. (27)
 
