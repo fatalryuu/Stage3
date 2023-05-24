f1db748e916d596ff203405e2d920e9b1aec86b3
DrEiZ3R <vanyax941@gmail.com>
2023-05-24 23:10:04 +0300

    feat: L1: Bartender, drinks! (46)

--- script.js
+++ script.js
@@ -655,7 +655,19 @@ function joinStrings(string1, string2){
 
 // L1: Bartender, drinks! (46)
 
+function getDrinkByProfession(param) {
+    param = param.toLowerCase();
 
+    switch(param) {
+        case "jabroni": return "Patron Tequila";
+        case "school counselor": return "Anything with Alcohol";
+        case "programmer": return "Hipster Craft Beer";
+        case "bike gang member": return "Moonshine";
+        case "politician": return	"Your tax dollars";
+        case "rapper": return "Cristal";
+        default: return "Beer";
+    }
+}
 
 // Well of Ideas - Easy Version (47)
 
